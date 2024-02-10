2babccd98de1d79c2b8af423676e14532207bccc
SonnySon1 <sonysudrajat94@gmail.com>
2024-02-10 16:57:30 +0700

    add function upload image

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Photo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,21 +11,28 @@ class UploadController extends Controller
 {
     //upload page
         public function index(){
-            return view('pages.upload');
+            $categories = Category::all();
+            return view('pages.upload', compact('categories'));
         }
 
     // upload store
         public function store(Request $request){
             $request->validate([
-                'photo_title' => 'required',
-                'photo_description' => 'required',
-                'category' => 'required',
+                'photo_title' => ['required'],
+                'photo_description' => ['required'],
+                'category' => ['required'],
+                'photo' => ['required', 'mimes:png,jpg,jpeg,gif']
             ]);
+
+            $photo_file = $request->file('photo');
+            $photo_extention = $photo_file->extension();
+            $photo_name = date('ymdhis').'.'.$photo_extention;
+            $photo_file->move(public_path('assets/img/img-e/'), $photo_name);
 
             $photo_data = [
                 'photo_title' => $request->photo_title,
                 'photo_description' => $request->photo_description,
-                'file_location' => $request->photo,
+                'file_location' => $photo_name,
                 'album_id' => $request->album,
                 'category_id' => $request->category,
                 'user_id' => Auth::user()->id,
